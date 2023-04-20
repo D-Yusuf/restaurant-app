dcdb68a5b84e32837a2016c08a21b45d15c26ae9
D-Yusuf <alnasiriyusuf@gmail.com>
2023-04-21 02:12:32 +0300

    Fixen an issue with git

---
+++ app/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        entry: 'main.js',
+      }
+    }
+    
+  }
+})
